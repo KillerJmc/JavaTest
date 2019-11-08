b2e8ccf6a45a7545d4fb5d537c6dba457a813610
KillerJmc <1934497212@qq.com>
2019-11-09 00:22:52 +0800

    test1

--- src/com/test/JDBC/JDBCTest.java
+++ src/com/test/JDBC/JDBCTest.java
@@ -82,6 +82,7 @@ public class JDBCTest {
             System.out.println(rs.getInt(1) + "---" + rs.getString(2) + "---" + rs.getString(3));
         }
         CloseUtils.closeAll(rs, ps, conn);
+        System.out.println("sdj");
     }
 
 }
