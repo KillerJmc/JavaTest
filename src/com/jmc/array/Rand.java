11e28a2366b7afae916868b8ed35dd8bccb942bf
KillerJmc <1934497212@qq.com>
2020-11-04 21:48:23 +0800

    interview, settings!

--- src/com/jmc/array/Rand.java
+++ src/com/jmc/array/Rand.java
@@ -18,43 +18,43 @@ public class Rand
     }
     
 	//生成不同的随机数
-    public static int[] getDiffRand(int min, int max, int n){
-		//如果参数不合法就返回
-        if (max < min || n > max - min + 1) return null;
+        public static int[] getDiffRand(int min, int max, int n){
+            //如果参数不合法就返回
+            if (max < min || n > max - min + 1) return null;
 
-		//rand数组
-        int[] rand = new int[n];
-		//已填入数字个数
-        int amount = 0;
+            //rand数组
+            int[] rand = new int[n];
+            //已填入数字个数
+            int amount = 0;
 
-		//如果最小值为0就填充数组(排除默认值0的干扰)
-        if (min == 0){
-			for (int i = 0; i < n; i++) {
-				rand[i] = Integer.MAX_VALUE;
-			}
-        }       
-
-		//当已填入的数字个数小于用户指定的个数
-        while (amount < n) {         
-			//生成用户指定范围的一个随机数
-            int result = nextInt(min, max);
-			//是否为新数字
-            boolean flag = true;
-
-			//遍历数组
-            for (int oldNum : rand){  
-				//如果不是新数字就跳出循环
-                if (result == oldNum){
-                    flag = false;
-                    break;
+            //如果最小值为0就填充数组(排除默认值0的干扰)
+            if (min == 0){
+                for (int i = 0; i < n; i++) {
+                    rand[i] = Integer.MAX_VALUE;
                 }
             }
 
-            //如果为新数字就记录
-            if (flag) {
-                rand[amount++] = result;
+            //当已填入的数字个数小于用户指定的个数
+            while (amount < n) {
+                //生成用户指定范围的一个随机数
+                int result = nextInt(min, max);
+                //是否为新数字
+                boolean flag = true;
+
+                //遍历数组
+                for (int oldNum : rand){
+                    //如果不是新数字就跳出循环
+                    if (result == oldNum){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                //如果为新数字就记录
+                if (flag) {
+                    rand[amount++] = result;
+                }
             }
+            return rand;
         }
-        return rand;
-    }
 }
---
+++ src/com/test/apply/thread/PrintInTern.java
@@ -0,0 +1,47 @@
+package com.test.apply.thread;
+
+import com.test.main.Main;
+
+import java.io.IOException;
+
+public class PrintInTern {
+    public static void main(String[] args) {
+        m();
+    }
+
+    private static int i = 1;
+    private static char c = 'A';
+
+    private static void m() {
+        Thread numT = new Thread(() -> {
+            synchronized (PrintInTern.class) {
+                while (i <= 26) {
+                    System.out.println("numT: " + i++);
+                    try {
+                        PrintInTern.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    PrintInTern.class.notifyAll();
+                }
+            }
+        }, "num");
+
+        Thread letterT = new Thread(() -> {
+            synchronized (PrintInTern.class) {
+                while (c <= 'Z') {
+                    System.out.println("letterT: " + c++);
+                    PrintInTern.class.notifyAll();
+                    try {
+                        PrintInTern.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "letter");
+
+        numT.start();
+        letterT.start();
+    }
+}
