fc0428ef53969128268534abb469104894be653c
Jmc <1934497212@qq.com>
2021-09-15 19:37:43 +0800

    Settings.

--- src/com/test/main/Main.java
+++ src/com/test/main/Main.java
@@ -4,7 +4,7 @@ package com.test.main;
  * @author Jmc
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
     }
 }
