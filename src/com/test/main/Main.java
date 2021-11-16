dee6a2794b30252f2176556410288f36aed1dd7f
Jmc <1934497212@qq.com>
2021-11-16 21:51:39 +0800

    C printf and scanf in Java!!!

--- src/com/test/main/Main.java
+++ src/com/test/main/Main.java
@@ -1,10 +1,13 @@
 package com.test.main;
 
+import com.jmc.io.Files;
+import com.test.incubator.clinker.Pointer;
+
 /**
  * @author Jmc
  */
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Throwable {
 
     }
 }
