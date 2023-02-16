0b736449063eaab493e559b16325b6cbdbbe27c9
Jmc <1934497212@qq.com>
2023-02-17 02:44:03 +0800

    Clear Main.java.

--- src/main/java/com/test/main/Main.java
+++ src/main/java/com/test/main/Main.java
@@ -1,34 +1,14 @@
 package com.test.main;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-
-class UserService {
-    public String getPassword(String name) {
-        if ("Jmc".equals(name)) {
-            return "123456";
-        }
-        return null;
-    }
-}
+import org.junit.Test;
 
 /**
  * @author Jmc
  */
 public class Main {
-    @Aspect
-    public static class MyAspect {
-        @Around("execution(* com.test.main.UserService.*(..))")
-        public Object getDamn(ProceedingJoinPoint joinPoint) throws Throwable {
-            var res = joinPoint.proceed();
-            return res + "???shiton";
-        }
-    }
+    @Test
+    public void main() {
 
-    public static void main(String... args) {
-        var userService = new UserService();
-        System.out.println(userService.getPassword("Jmc"));
     }
 }
 
